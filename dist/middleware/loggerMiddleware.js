0b9c53c9204c5fb1d2331e2bb85ef0484d0a81ef
Franco Minati <100171822+FrancoMinati@users.noreply.github.com>
2024-09-26 03:18:21 -0300

    Delete dist directory

--- dist/middleware/loggerMiddleware.js
+++
@@ -1,9 +0,0 @@
-"use strict";
-Object.defineProperty(exports, "__esModule", { value: true });
-exports.logger = void 0;
-const logger = (req, res, next) => {
-    console.log(`${req.method} request for '${req.url}'`);
-    next(); // Pass control to the next middleware or route handler
-};
-exports.logger = logger;
-//# sourceMappingURL=loggerMiddleware.js.map
